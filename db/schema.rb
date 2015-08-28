a5d2d70e0ebaa62d4b02af00342eca27e2455859
Wilhelm <va3093@gmail.com>
2015-08-28 17:31:34 +0100

    added the brunch logo to the main template.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823150546) do
+ActiveRecord::Schema.define(version: 20150823154004) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,11 +35,11 @@ ActiveRecord::Schema.define(version: 20150823150546) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "first_name"
-    t.string   "surname"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "first_name", limit: 25
+    t.string   "last_name",  limit: 50
+    t.string   "email",                 default: "", null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
   end
 
 end
