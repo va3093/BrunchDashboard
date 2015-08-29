d10c99553b1477091d60f6dc56e1659251a6023b
Wilhelm <va3093@gmail.com>
2015-08-29 09:33:46 +0100

    Added some view elements to the sign up page .

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'signup/index'
-
-  root 'welcome#index'
+  get "/signup", to: "signup#index" 
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
