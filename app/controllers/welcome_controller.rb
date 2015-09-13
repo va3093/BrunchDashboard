c160ccfe50e7c247ada30c5769b80abf74f2e644
Wilhelm <va3093@gmail.com>
2015-09-13 15:25:28 +0100

    Displaying tables for the current month and all the users that are associated with the appropriate week.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
 		@user = User.find_by(id: session[:current_user_id])
 		puts @user
 		if @user 
+			@events = Event.eventsForCurrentMonth
 			@users = User.all
 		else 
 			redirect_to :controller => 'signup', :action => 'index'
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,33 @@
 class Event < ActiveRecord::Base
+
+	def self.eventsForMonth(month, year)
+		events = []
+		for i in 1	..5
+			eventDate = Event.date_for_nth_sunday(i,month,year)
+			if eventDate 
+				event = Event.find_or_create_by(date: eventDate)
+				events << event
+			end
+		end
+		events
+	end
+
+	def self.eventsForCurrentMonth
+		Event.eventsForMonth(Time.now.month, Time.now.year)
+	end
+
+	def self.date_for_nth_sunday(n,month,year)
+	  # get the last day of the month
+	  date = Date.new year, month, 1
+	  #subtract number of days we are ahead of sunday
+	  date += ((7 * n) - date.wday) 
+	  if n > 1 && date.day < 7
+	  	return
+	  else
+	  	date
+	  end
+
+	end
   
   has_and_belongs_to_many :users
 end
