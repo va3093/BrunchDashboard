a2d331df099e60c37ee9bd217515f464c736c9a0
Wilhelm <va3093@gmail.com>
2015-10-17 13:19:46 +0100

    Fixed the css for the month toggles and added the full div links that will load up previous or next month views.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,19 @@
 class WelcomeController < ApplicationController
 	def index
 		@user = User.find_by(id: session[:current_user_id])
-		puts @user
+		@monthToShowString = params[:month]
+		
 		if @user
-			# @events = Event.eventsForCurrentMonth
-			@events = Event.where("date >= :today", {today: Date.today})
-			@prevMonth = 1.month.ago.strftime("%B")
-			@nextMonth = 1.month.from_now.strftime("%B")
-			@users = User.all
+			if @monthToShowString
+				@events = Event.eventsForMonth(@monthToShowString, Time.now.year)
+			else
+				@events = Event.eventsForCurrentMonth
+				#@events = Event.where("date >= :today", {today: Date.today})
+				@prevMonth = 1.month.ago.strftime("%B")
+				@nextMonth = 1.month.from_now.strftime("%B")
+				@users = User.all
+			end
+
 		else
 			redirect_to :controller => 'signup', :action => 'index'
 		end
