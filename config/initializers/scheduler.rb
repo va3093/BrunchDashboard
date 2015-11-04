a80d97b1ea803196b9f6d02bfe5954ae8a3f4089
Wilhelm <va3093@gmail.com>
2015-11-04 08:29:36 +0000

    silencing scheduler for the time being.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,6 +4,6 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
 
 
-scheduler.every '1m' do
-  UserMailer.monthlySignupReminder().deliver_now
-end
+# scheduler.every '1m' do
+#   UserMailer.monthlySignupReminder().deliver_now
+# end
