03b2b9c29c4c66867ac8ce5e9c249eaab82f6204
Wilhelm <va3093@gmail.com>
2015-11-09 19:06:23 +0000

    fixed bug that was causing app to crash when going to next or previous year.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,18 +3,16 @@ class WelcomeController < ApplicationController
 		@monthToShowString = params[:month]
 		puts params
 		if user_signed_in?
-			if @monthToShowString
+			@monthToShowString = params[:month] || Date::MONTHNAMES[Time.now.month]
 				monthInt = Date::MONTHNAMES.index(@monthToShowString)
-				@events = Event.eventsForMonth(monthInt, Time.now.year)
-				@prevMonth = Date::MONTHNAMES[monthInt - 1]
-				@nextMonth = Date::MONTHNAMES[monthInt + 1]
-			else
-				@events = Event.eventsForCurrentMonth
-				#@events = Event.where("date >= :today", {today: Date.today})
-				@prevMonth = 1.month.ago.strftime("%B")
-				@nextMonth = 1.month.from_now.strftime("%B")
-				@users = User.all
-			end
+				currentYear = (params[:year] || Time.now.year).to_i
+				@events = Event.eventsForMonth(monthInt, currentYear)
+				@prevMonth = Date::MONTHNAMES[(monthInt - 1)%13] || "December"
+				@nextMonth = Date::MONTHNAMES[(monthInt + 1)%13] || "January"
+				@prevMonthYear = (monthInt == 1 ? (currentYear - 1).to_s : currentYear).to_s
+				@nextMonthYear = (monthInt == 12 ? (currentYear + 1).to_s : currentYear).to_s
+
+				 
 		else
 			redirect_to :controller => 'signup', :action => 'index'
 		end
