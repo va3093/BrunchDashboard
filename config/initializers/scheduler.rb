edfd6965c2a1027632e5287b11b621aeb973e7fe
Wilhelm <va3093@gmail.com>
2016-07-01 11:48:04 +0100

    Re-added monthly sign up reminder.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,13 +3,13 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-# scheduler.cron('00 12 * * sun#-1') do
-# 	users = User.all
-#     events = 	Event.eventsForMonth(Time.now.month + 1, Time.now.year) 
-#     users.each do |user|
-# 		UserMailer.monthlySignupReminder(user,events ).deliver
-#     end
-# end
+scheduler.cron('00 12 * * sun#-1') do
+	users = User.all
+    events = 	Event.eventsForMonth(Time.now.month + 1, Time.now.year) 
+    users.each do |user|
+		UserMailer.monthlySignupReminder(user,events ).deliver
+    end
+end
 
 scheduler.cron('00 12 * * TUE') do
 	Event.remindUsersAboutUpcommingEvent()
