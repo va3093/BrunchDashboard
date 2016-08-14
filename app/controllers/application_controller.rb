235c9edf2b4ade80582a663d0712a5858e6edd7d
Wilhelm <va3093@gmail.com>
2016-08-14 09:26:26 +0100

    Added events and small bug fix.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  def analytics
+    @analytics ||= Analytics.new(current_user, google_analytics_client_id)
+  end
+
+  def google_analytics_client_id
+    google_analytics_cookie.gsub(/^GA\d\.\d\./, '')
+  end
+
+  def google_analytics_cookie
+    cookies['_ga'] || ''
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
